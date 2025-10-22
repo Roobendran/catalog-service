2031a5abfb2c572de2a25554c97f4c33a3da262f
Roobendran K <alaguroobendrank@gmail.com>
2025-10-23 01:50:14 +0200

    Schema init docker setup

---
+++ data/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE products (
+    product_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE,
+    online_status VARCHAR(10),
+    long_description TEXT,
+    short_description VARCHAR(500)
+);
+
+CREATE TABLE categories (
+    category_id BIGINT NOT NULL PRIMARY KEY,
+    category_name VARCHAR(255),
+    parent_category_id BIGINT NULL
+);
+
+CREATE TABLE product_category_map (
+    product_id BIGINT NOT NULL,
+    category_id BIGINT NOT NULL,
+    PRIMARY KEY (product_id, category_id)
+);
